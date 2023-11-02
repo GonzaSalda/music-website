bf2e77ff095c8099077441d40911036827b458a5
Gonzalo <tomas_salda@hotmail.com>
2023-11-02 18:02:42 -0300

    Creando el hero

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,10 @@
 import Events from '@/components/events/Events';
+import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
     <main>
+      <Hero/>
       <Events/>
       <div className='h-[4000px]'></div>
     </main>
