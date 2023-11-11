f5c7e8eb713f5e78316ddb4df150056b0acb75ff
Gonzalo <tomas_salda@hotmail.com>
2023-11-10 21:04:11 -0300

    modificando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,54 @@
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+"use client"
+import {useEffect, useState} from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
-export default Header
+import {motion} from 'framer-motion'
+import { fadeIn } from '../../variants'
+import NavMobile from './NavMobile'
+import Nav from './Nav'
+
+const Header = () => {
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+  
+    const handleScroll = () => {
+      setActive(window.scrollY > 100)
+    }
+    window.addEventListener('scroll' , handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll' , handleScroll)
+    }
+  }, [])
+  
+
+  return( 
+  <header 
+  className={`fixed z-50 w-full ${
+    active ? 'bg-[#030315] py-8': 'bg-transparent py-8'
+  }`}
+  >
+    <div className="container mx-auto flex flex-col xl:flex-row items-center justify-between">
+      {/* Logo */}
+      <Link 
+      href={'#'} 
+      className='relative flex w-[226px] h-[37.64px] transition-all mb-4 xl:mb-0'
+      >
+        <Image src={'/assets/header/logo.svg'} fill/>
+      </Link>
+      {/* nav */}
+    <Nav/>
+    {/* nav mobile */}
+    <NavMobile/>
+    {/* menu btn */}
+    <div>menu btn</div>
+    {/* social */}
+    <div>social icons</div>
+    </div>
+  </header>
+  );
+};
+
+export default Header;
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -9,16 +9,23 @@ import { fadeIn } from "../../variants"
 const Player = () => {
   return (
     <>
-        <div className="bg-gradient-to-r from-tertiary/70 to-primary/10 backdrop-blur-[15px] h-[112px] flex items-center relative z-40">
+        <motion.div
+        variants={fadeIn('up', 0.1)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once:false, amount:0.1}}
+        className="bg-gradient-to-r from-tertiary/70 to-primary/10 backdrop-blur-[15px] h-[112px] flex items-center relative z-40">
             <div className="container mx-auto flex  flex-col justify-between items-center xl:flex-row">
                 {/* text & avatar img */}
                 <div className="hidden w-[300px] xl:flex items-center gap-x-4">
+                    {/* Avatar Img */}
                     <div className="relative w-16 h-16">
                     <Image 
                     src={'/assets/player/avatar.png'}
                     fill
                     priority/>
                     </div>
+                    {/* Text */}
                     <div className="leading-none">
                         <div className="text-lg font-medium">Mia Reynolds</div>
                         <div className="text-sm font-light">Freedom</div>
@@ -36,12 +43,12 @@ const Player = () => {
                     style={{
                         background:'transparent',
                         boxShadow:'none',
-                        width:'100%'
+                        width:'100%',
                     }}
                     />
                 </div>
             </div>
-        </div>
+        </motion.div>
     </>
   )
   
