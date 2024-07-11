0705f4b351ad92dacc96e4983af58087baa64843
GonzaSalda <gonza.salda00@gmail.com>
2024-07-11 00:37:34 -0300

    Add NavContextProvider and update MenuBtn and NavMobile components

--- src/components/MenuBtn.jsx
+++ src/components/MenuBtn.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import { NavContext } from "@/context/NavContext";
+import React, { useContext } from "react";
 
 const MenuBtn = () => {
+  const { setIsOpen } = useContext(NavContext);
   return (
-    <div className='group flex flex-col gap-y-2 cursor-pointer xl:hidden group items-end'>
-        <div className='w-7 h-[2px] bg-white  transition-all'></div>
-        <div className='w-4 group-hover:w-7 h-[2px] bg-white'></div>
-        <div className='w-7 h-[2px] bg-white'></div>
+    <div
+      onClick={() => setIsOpen(true)}
+      className="group flex flex-col gap-y-2 cursor-pointer xl:hidden group items-end"
+    >
+      <div className="w-7 h-[2px] bg-white  transition-all"></div>
+      <div className="w-4 group-hover:w-7 h-[2px] bg-white"></div>
+      <div className="w-7 h-[2px] bg-white"></div>
     </div>
-  )
-}
+  );
+};
 
-export default MenuBtn
+export default MenuBtn;
--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { NavContext } from "@/context/NavContext";
+import React, { useContext } from "react";
+import { RiCloseLine } from "react-icons/ri";
+import Nav from "./Nav";
 
 const NavMobile = () => {
-  return <nav className='xl:hidden'>NavMobile</nav>
-  
-}
+  const { isOpen,setIsOpen } = useContext(NavContext);
+  return (
+    <nav
+      className={`${
+        isOpen ? 'right-0' : '-right-full'}
+        xl:hidden fixed bg-primary w-full top-0 z-20 bottom-0 transition-all duration-500`}
+    >
+      <div className=" absolute right-4 top-5 cursor-pointer" onClick={() => setIsOpen(false)}>
+        <RiCloseLine className="text-5xl" />
+      </div>
+      <Nav containerStyles='flex flex-col text-[30px] uppercase font-bold bg-pink-500/10 h-full items-center justify-center gap-y-8'/>
+    </nav>
+  );
+};
 
-export default NavMobile
+export default NavMobile;
