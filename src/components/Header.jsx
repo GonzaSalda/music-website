d3efb0327e8e8f3f5cec17c7aff06423c3e440b8
GonzaSalda <gonza.salda00@gmail.com>
2024-07-11 00:05:10 -0300

    Refactor header component and add menu button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,53 +1,58 @@
-"use client"
-import {useEffect, useState} from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-import {motion} from 'framer-motion'
-import { fadeIn } from '../../variants'
-import NavMobile from './NavMobile'
-import Nav from './Nav'
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+import NavMobile from "./NavMobile";
+import Nav from "./Nav";
+import MenuBtn from "./MenuBtn";
 
 const Header = () => {
   const [active, setActive] = useState(false);
 
   useEffect(() => {
-  
     const handleScroll = () => {
-      setActive(window.scrollY > 100)
-    }
-    window.addEventListener('scroll' , handleScroll)
+      setActive(window.scrollY > 100);
+    };
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll' , handleScroll)
-    }
-  }, [])
-  
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-  return( 
-  <header 
-  className={`fixed z-50 w-full ${
-    active ? 'bg-[#030315] py-8': 'bg-transparent py-8'
-  }`}
-  >
-    <div className="container mx-auto flex flex-col xl:flex-row items-center justify-between">
-      {/* Logo */}
-      <Link 
-      href={'#'} 
-      className='relative flex w-[226px] h-[37.64px] transition-all mb-4 xl:mb-0'
-      >
-        <Image src={'/assets/header/logo.svg'} fill/>
-      </Link>
-      {/* nav */}
-    <Nav/>
-    {/* nav mobile */}
-    <NavMobile/>
-    {/* menu btn */}
-    <div>menu btn</div>
-    {/* social */}
-    <div>social icons</div>
-    </div>
-  </header>
+  return (
+    <header
+      className={`fixed z-50 w-full transition-all ${
+        active ? "bg-[#030315] py-8" : "bg-transparent py-8"
+      }`}
+    >
+      <div className="container mx-auto flex flex-col xl:flex-row items-center justify-between">
+        {/* Logo */}
+        <Link
+          href={"#"}
+          className="relative flex w-[226px] h-[37.64px] transition-all mb-4 xl:mb-0"
+        >
+          <Image src={"/assets/header/logo.svg"} fill />
+        </Link>
+
+        {/* nav */}
+        <Nav containerStyles="hidden xl:flex items-center gap-x-8" />
+
+        {/* nav mobile */}
+        <NavMobile />
+
+        <div className="absolute right-7 top-9 z-10 xl:hidden">
+          <MenuBtn />
+        </div>
+      
+
+        {/* social */}
+        <div>social icons</div>
+      </div>
+    </header>
   );
 };
 
